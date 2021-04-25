19699195bf9cad5ca9966ca6c8ae646c18e02f08
Davide Cerato <mrc3drik@gmail.com>
2021-04-25 12:57:53 +0200

    addProduct function

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,43 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint public productsCount = 0;
+    mapping(uint => Product) public products; 
+
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool forSale;
+    }
+
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool forSale
+    );
 
     constructor() public {
         name = "Mr C3drik Marketplace";
     }
+
+    function addProduct(string memory _name, uint _price) public {
+        // assert provided variables are valid
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
+        // increase product counter
+        productsCount ++;
+
+        // add product
+        products[productsCount] = Product(productsCount, _name, _price, msg.sender, true);
+
+        // trigger an event to add the product to the blockchain
+        emit ProductCreated(productsCount, _name, _price, msg.sender, true);
+    }
+
+
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,5 +1,6 @@
 const { assert } = require("chai")
 const Marketplace = artifacts.require("./Marketplace.sol")
+require("chai").use(require("chai-as-promised")).should()
 
 contract("Marketplace", (accounts) => {
     var marketplace
@@ -20,6 +21,42 @@ contract("Marketplace", (accounts) => {
         it("has a name", async () => {              // assert that the name is correct
             let name = await marketplace.name()
             assert.equal(name, "Mr C3drik Marketplace")
+        })
+    })
+
+    describe("products", async() => {
+        var result, productsCount
+        let name = "Test product"
+        let price = web3.utils.toWei("1", "Ether")
+
+        before(async () => {
+            result = await marketplace.addProduct(name, price, {from: accounts[1]})
+            productsCount = await marketplace.productsCount()
+        })
+
+        it("adds product", async() => {             // assert that products get added correctly
+            var event = result.logs[0].args
+
+            // check for successfull results
+            assert.equal(productsCount, 1)
+            assert.equal(event.id.toNumber(), productsCount, "id correct")
+            assert.equal(event.name, name, "name correct")
+            assert.equal(event.price, price, "price correct")
+            assert.equal(event.owner, accounts[1], "owner correct")
+            assert.equal(event.forSale, true, "forSale correct")
+
+            // check for erroneous results
+            await marketplace.addProduct("", price, {from: accounts[1]}).should.be.rejected
+            await marketplace.addProduct(name, 0, {from: accounts[1]}).should.be.rejected;
+        })
+
+        it("lists products", async() => {
+            var product = await marketplace.products(productsCount)
+            assert.equal(product.id.toNumber(), productsCount, "id correct")
+            assert.equal(product.name, name, "name correct")
+            assert.equal(product.price, price, "price correct")
+            assert.equal(product.owner, accounts[1], "owner correct")
+            assert.equal(product.forSale, true, "forSale correct")
         })
     })
 })
